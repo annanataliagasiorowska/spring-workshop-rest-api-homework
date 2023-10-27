26c57c81856c2a832fa4aa4adef12c93599a9f18
Anna Gąsiorowska <annanataliagasiorowska@gmail.com>
2023-10-27 11:21:38 +0200

    add annotations for Open API documentation in ArticleController

--- src/main/java/pl/dgadecki/springworkshoprestapi/business/article/domain/controller/ArticleController.java
+++ src/main/java/pl/dgadecki/springworkshoprestapi/business/article/domain/controller/ArticleController.java
@@ -1,14 +1,35 @@
 package pl.dgadecki.springworkshoprestapi.business.article.domain.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 import pl.dgadecki.springworkshoprestapi.business.article.domain.service.ArticleService;
 import pl.dgadecki.springworkshoprestapi.business.article.dto.Article;
 import pl.dgadecki.springworkshoprestapi.business.article.dto.api.*;
 
-import java.util.List;
-
+@Tag(name = "Article API", description = "All operations available for articles")
+@ApiResponses({
+        @ApiResponse(responseCode = "200", description = "The articles were successfully found", content = {
+                @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = FindAllArticlesResponse.class))
+        }),
+        @ApiResponse(responseCode = "400", description = "Bad request", content = {
+                @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorResponse.class))
+        }),
+        @ApiResponse(responseCode = "401", description = "Unauthorized", content = {
+                @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorResponse.class))
+        }),
+        @ApiResponse(responseCode = "500", description = "Internal server error", content = {
+                @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorResponse.class))
+        })
+})
 @RestController
 @RequestMapping("/api/v1/articles")
 public class ArticleController {
@@ -18,49 +39,58 @@ public class ArticleController {
         this.articleService = articleService;
     }
 
-    @GetMapping
+
+    @Operation(summary = "Find all articles", description = "Returns all articles from the system")
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public FindAllArticlesResponse findAllArticles() {
         return FindAllArticlesResponse.fromArticlesList(articleService.findAllArticles());
     }
 
-    @PostMapping
-    public CreateArticleResponse createArticle(@RequestBody CreateArticleRequest createArticleRequest) {
+    @Operation(summary = "Create article", description = "Given data from user creates new article in the system")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public CreateArticleResponse createArticle(@Parameter(description = "Data passed to create article") @RequestBody CreateArticleRequest createArticleRequest) {
         Article createdArticle = articleService.saveArticle(createArticleRequest.toArticle());
         return CreateArticleResponse.fromArticle(createdArticle);
     }
 
-    @PutMapping("/{id}")
-    public UpdadeArticleResponse updateArticle(@PathVariable("id") Long articleId,
-                              @RequestBody UpdateArticleRequest updateArticleRequest) {
+    @Operation(summary = "Find article by id", description = "Given a valid id it returns proper article, otherwise error")
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public FindArticleResponse findArticle(@Parameter(description = "Identifier of the article to be found") @PathVariable("id") Long articleId) {
+        return FindArticleResponse.fromArticle(articleService.fetchArticleById(articleId));
+    }
+
+    @Operation(summary = "Update article", description = "Updates article found by id using data from request")
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public UpdadeArticleResponse updateArticle(@Parameter(description = "Identifier of the article to be updated") @PathVariable("id") Long articleId,
+                              @Parameter(description = "Data passed to update article") @RequestBody UpdateArticleRequest updateArticleRequest) {
         Article articleToUpdate = articleService.updateArticle(articleId, updateArticleRequest.toArticle());
         return UpdadeArticleResponse.fromArticle(articleToUpdate);
     }
 
-    @DeleteMapping("/{id}")
-    public void deleteArticle(@PathVariable("id") Long articleId) {
+    @Operation(summary = "Delete article", description = "Deletes article by given id")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(value =  "/{id}")
+    public void deleteArticle(@Parameter(description = "Identifier of the article to be deleted") @PathVariable("id") Long articleId) {
         articleService.deleteArticle(articleId);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Article> findArticle(@PathVariable("id") Long articleId) {
-        return new ResponseEntity<>(articleService.fetchArticleById(articleId),
-                HttpStatus.OK);
-    }
-
-    @GetMapping("/search")
-    public ResponseEntity<List<Article>> findArticleByNameAndDescription(
+    @Operation(summary = "Find articles by name and/or description",
+            description = "Given a name, description or both parameters, it will find article or articles which name and description contain searched phrases")
+    @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
+    public FindAllArticlesResponse findArticleByNameAndDescription(
             @RequestParam(name="name", required = false) String name,
             @RequestParam(name="description", required = false) String description) {
-        return new ResponseEntity<>(
-                articleService.findArticleByNameAndDescription(name, description),
-                HttpStatus.OK);
+        return FindAllArticlesResponse.fromArticlesList(
+                articleService.findArticleByNameAndDescription(name, description));
     }
 
-    @GetMapping("/producer")
-    public ResponseEntity<List<Article>> findArticleByProducer(
-            @RequestParam(name="producer") String producer) {
-        return new ResponseEntity<>(articleService.findByProducer(producer),
-                HttpStatus.OK);
+    @Operation(summary = "Find articles by producer",
+            description = "Given a producer it will find article or articles which contains searched phrase in producer")
+    @GetMapping(value = "/producer", produces = MediaType.APPLICATION_JSON_VALUE)
+    public FindAllArticlesResponse findArticleByProducer(
+            @RequestParam(name="producer", required = false) String producer) {
+        return FindAllArticlesResponse.fromArticlesList(articleService.findByProducer(producer));
     }
 
 
---
+++ src/main/java/pl/dgadecki/springworkshoprestapi/configuration/ApiDocumentationConfig.java
@@ -0,0 +1,61 @@
+package pl.dgadecki.springworkshoprestapi.configuration;
+
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class ApiDocumentationConfig {
+
+    @Bean
+    public OpenAPI openApi() {
+        Contact contact = new Contact()
+                .name("Anna Gasiorowska")
+                .email("annagasiorowska.it@gmail.com");
+
+        License license = new License()
+                .name("Apache 2.0")
+                .url("http://www.apache.org/licenses/LICENSE-2.0.html");
+
+        Info info = new Info()
+                .title("Workshop-homework")
+                .description("API prepared as a homework for Spring Boot Workshop")
+                .version("1.0.0")
+                .license(license)
+                .contact(contact);
+
+        List<Server> servers = List.of(
+                new Server().url("http://localhost:8080").description("Local"),
+                new Server().url("http://workshop-dev.pl").description("Development"),
+                new Server().url("http://workshop-prod.pl").description("Production"),
+                new Server().url("http://workshop-test.pl").description("Test")
+        );
+
+        return new OpenAPI().info(info).servers(servers);
+    }
+    @Bean
+    public GroupedOpenApi articlesOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("Articles")
+                .pathsToMatch("api/v1/articles/**")
+                .build();
+    }
+    @Bean
+    public GroupedOpenApi customersOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("Customers")
+                .pathsToMatch("api/v1/customers/**")
+                .build();
+    }
+
+
+
+
+}
